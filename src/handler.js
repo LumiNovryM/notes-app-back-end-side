37566bf1deeff60c8fcf29b7dd8568ee04b5575b
LumiNovryM <luminovrymekel@gmail.com>
2023-04-02 03:32:47 +0700

    Menampilkan Catatan

---
+++ src/handler.js
@@ -0,0 +1,67 @@
+const { nanoid } = require("nanoid");
+const notes = require("./notes");
+
+const addNoteHandler = (request, h) => {
+    const { title, tags, body } = request.payload;
+
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updateAt = createdAt;
+
+    const newNote = {
+        title, tags, body, id, createdAt, updateAt,
+    };
+
+    notes.push(newNote);
+
+    const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+    if (isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan berhasil ditambahkan',
+            data: {
+                noteId: id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Catatan gagal ditambahkan',
+    });
+    response.code(500);
+    return response;
+};
+ 
+const getAllNotesHandler = () => ({
+    status: 'success',
+    data: {
+        notes,
+    },
+});
+
+const getNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const note = notes.filter((n) => n.id === id)[0];
+
+    if (note !== undefined) {
+        return {
+          status: 'success',
+          data: {
+            note,
+          },
+        };
+    }
+    const response = h.response({
+        status: 'fail',
+        message: 'Catatan tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require("./handler");
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+];
+
+module.exports = routes;
